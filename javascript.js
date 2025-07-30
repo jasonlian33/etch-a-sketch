c25671c580c14f4aa828d9ad36349f9b8b416238
jason lian <jasonlian16@gmail.com>
2025-07-29 23:26:42 -0400

    created 16x16 grid using javascript dom

---
+++ javascript.js
@@ -0,0 +1,33 @@
+console.log("working?");
+
+const container = document.querySelector(".container");
+console.log(container);
+
+
+function createRow(){
+    for (let i = 0; i < 16; i++){
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+    }  
+}
+
+createRow();
+
+const row = document.querySelectorAll(".row");
+function createGrid(){
+    row.forEach(row =>{
+        for (let i = 0; i < 16; i++){
+            const column = document.createElement("div");
+            column.classList.add("column");
+            //column.textContent = "Hi"
+            row.appendChild(column);
+        }  
+    })
+}
+
+createGrid();
+
+
+
+
