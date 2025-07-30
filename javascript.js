ad1e16aa771bf8e3da1260015cd87b9523173d64
jason lian <jasonlian16@gmail.com>
2025-07-30 00:25:48 -0400

    started button section

--- javascript.js
+++ javascript.js
@@ -1,23 +1,25 @@
 console.log("working?");
 
+const defaultSize = 16;
+
 const container = document.querySelector(".container");
 console.log(container);
 
 
-function createRow(){
-    for (let i = 0; i < 50; i++){
+function createRow(defaultSize){
+    for (let i = 0; i < defaultSize; i++){
         const row = document.createElement("div");
         row.classList.add("row");
         container.appendChild(row);
     }  
 }
 
-createRow();
+createRow(defaultSize);
 
 const row = document.querySelectorAll(".row");
-function createGrid(){
+function createGrid(defaultSize){
     row.forEach(row =>{
-        for (let i = 0; i < 50; i++){
+        for (let i = 0; i < defaultSize; i++){
             const column = document.createElement("div");
             column.classList.add("column");
             //column.textContent = "Hi"
@@ -26,7 +28,7 @@ function createGrid(){
     })
 }
 
-createGrid();
+createGrid(defaultSize);
 const columns = document.querySelectorAll(".column");
 columns.forEach(column => {
     column.addEventListener("mouseover", () => {
@@ -34,6 +36,16 @@ columns.forEach(column => {
     column.style["background-color"] = "grey";
     });
 });
+
+const resetButton = document.querySelector(".reset-button");
+resetButton.addEventListener('click', function(){
+    let newSize = prompt("Enter a number for the new grid size");
+
+    //createRow(newSize);
+    //createGrid(newSize);
+    defaultSize = newSize;
+
+})
 
 /*
 columns.forEach(column => {
